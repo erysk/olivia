00a1f0a7621e3253ae04e597323fb65abb4371b2
eRy-sk <ride.poke@gmail.com>
2019-08-01 20:06:47 +0900

    Fix

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 
 require 'mobb'
 
+# shellで対話する場合はコメントアウトする
 set :service, 'slack'
 
 helpers do
@@ -9,20 +10,12 @@ helpers do
     # 何かゴニョゴニョするならここで
     '@channel そろそろリモートもくもく会が始まるのでは'
   end
-
-  def time_report
-    "#{Time.now.hour}時をお伝えします。"
-  end
 end
 
 on(/^olivia ping/) do
   'pong'
 end
 
-cron '50 20 * * 6' do
+cron '0 19 * * 6', dest_to: 'リモートもくもく会' do
   remind_mokumoku
-end
-
-cron '0 * * * *' do
-  time_report
 end
